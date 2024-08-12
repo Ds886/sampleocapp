5a4476304add21f4a95273134b7497607d3c4227
root <root@localhost.localnet>
2024-08-13 02:03:12 +0300

    * adds postgres

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 var http = require('http');
+async function ConnectDB(){
+  const { Client } = require('pg')
+  const client = new Client({
+  	user: process.env.PGUSER,
+  	password: process.env.PGPASSWORD,
+  	host: process.env.PGHOST,
+  	port: process.env.PGPORT,
+  });
+  await client.connect()
+
+  try {
+     const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+     console.log(res.rows[0].message) // Hello world!
+  } catch (err) {
+     console.error(err);
+  } finally {
+     await client.end()
+  }
+}
+ConnectDB()
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
